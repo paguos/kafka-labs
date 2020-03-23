4e4a30d9cb814e0bb3ea512ecc9d12173fdd144b
Pablo Osinaga <paguos@gmail.com>
2020-03-23 12:56:41 +0100

    fix: port forward

--- scripts/port-forward.sh
+++ scripts/port-forward.sh
@@ -14,4 +14,7 @@ info "zookeeper on http://localhost:2181"
 kubectl -n kafka port-forward service/cp-cp-zookeeper 2181:2181 &
 
 info "kafka-connect on http://localhost:8083"
-kubectl -n kafka port-forward service/cp-cp-kafka-connect 8083:8083 
+kubectl -n kafka port-forward service/cp-cp-kafka-connect 8083:8083 &
+
+info "msqll on http://localhost:1433"
+kubectl -n kafka port-forward service/mssql-mssql-linux 1433:1433 
